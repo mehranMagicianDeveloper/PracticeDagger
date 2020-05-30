9788532e74d741ae24a218f4fd3fdb78be3f77f2
BlackPearl <mehran.blackpearl@gmail.com>
2020-05-30 13:51:27 +0430

    adding basic dagger

---
+++ app/src/main/java/net/kurdsofts/practicedagger/di/AppComponent.java
@@ -0,0 +1,27 @@
+package net.kurdsofts.practicedagger.di;
+
+import android.app.Application;
+
+import net.kurdsofts.practicedagger.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class,
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        Builder application (Application application);
+
+        AppComponent build();
+    }
+}
