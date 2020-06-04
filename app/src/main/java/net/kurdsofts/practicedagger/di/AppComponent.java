f9579752a27806393818e415478cacbf5c3286ec
BlackPearl <mehran.blackpearl@gmail.com>
2020-06-04 12:21:08 +0430

    fix: dagger building

--- app/src/main/java/net/kurdsofts/practicedagger/di/AppComponent.java
+++ app/src/main/java/net/kurdsofts/practicedagger/di/AppComponent.java
@@ -15,7 +15,6 @@ import dagger.android.support.AndroidSupportInjectionModule;
                 ActivityBuildersModule.class,
                 AppModule.class,
                 ViewModelFactoryModule.class,
-
         }
 )
 public interface AppComponent extends AndroidInjector<BaseApplication> {
--- app/src/main/java/net/kurdsofts/practicedagger/di/ViewModelFactoryModule.java
+++ app/src/main/java/net/kurdsofts/practicedagger/di/ViewModelFactoryModule.java
@@ -5,7 +5,9 @@ import androidx.lifecycle.ViewModelProvider;
 import net.kurdsofts.practicedagger.viewmodels.ViewModelProviderFactory;
 
 import dagger.Binds;
+import dagger.Module;
 
+@Module
 public abstract class ViewModelFactoryModule {
 
     @Binds
